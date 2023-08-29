9501de6e178d471fa770ac796b58845de5bf4500
Md.Rezwan <87122824+Rezwan0000000@users.noreply.github.com>
2023-08-29 18:50:12 +0600

    Update Hash_crack.py

--- Hash_crack.py
+++ Hash_crack.py
@@ -1,6 +1,7 @@
 import hashlib
+import base58
+import base64
 import os
-
 def find_hash_match(hash_type, hash_value, wordlist_path):
     with open(wordlist_path, 'r', errors='ignore') as wordlist:
         for line in wordlist:
@@ -12,11 +13,13 @@ def find_hash_match(hash_type, hash_value, wordlist_path):
 
 def detect_hash_type(hash_value):
     hash_lengths = {
+        16: "md2",
         32: "md5",
         40: "sha1",
         64: "sha256",
         56: "sha224",
-        96: "sha384"
+        96: "sha384",
+        128: "sha512"
     }
     
     hash_length = len(hash_value)
@@ -26,30 +29,76 @@ def decode_hash(hash_type, hash_value, wordlist_path):
     plaintext = find_hash_match(hash_type, hash_value, wordlist_path)
     return plaintext
 
+def decode_base32(encoded_value):
+    return base64.b32decode(encoded_value).decode()
+
+def decode_base45(encoded_value):
+    return base64.b45decode(encoded_value).decode()
+
+def decode_base58(encoded_value):
+    return base58.b58decode(encoded_value).decode()
+
+def decode_base64(encoded_value):
+    return base64.b64decode(encoded_value).decode()
+
 def clear_terminal():
     os.system('clear')  # For Linux/macOS
     # Alternatively, you can use os.system('cls') for Windows
 
+def print_figlet_banner(text):
+    os.system(f'figlet -c {text}')
+
 if __name__ == "__main__":
     clear_terminal()
-    print("                            Hash Decoder and Matcher")
+    print_figlet_banner("Hash Decoder")
     print("                      +------------------------------------+")
     print("                      |     Hash crack using python        |")
     print("                      |     Created By Rezwan              |")
-    print("                      +------------------------------------+")
- 
-    hash_value = input("Enter the hash value: ")
-    detected_hash_type = detect_hash_type(hash_value)
+    print("                      +------------------------------------+\n") 
+    print("-----------------------------------------------------------------\n")
     
-    if detected_hash_type == "unknown":
-        print("Unknown hash type based on hash length.")
-    else:
-        print(f"Detected hash type: {detected_hash_type}")
-        
+    hash_value = input("Enter the hash or encoded value: ")
+    
+    decoded_value = None
+    hash_type = detect_hash_type(hash_value)
+    
+    if hash_type != "unknown":
+        print(f"Detected hash type: {hash_type}")
         wordlist_path = "/usr/share/wordlists/rockyou.txt"
-        plaintext = decode_hash(detected_hash_type, hash_value, wordlist_path)
+        decoded_value = decode_hash(hash_type, hash_value, wordlist_path)
+    else:
+        try:
+            decoded_value = decode_base32(hash_value)
+            print("Decoded using Base32:")
+        except:
+            pass
         
-        if plaintext:
-            print(f"\nDecoded plaintext: {plaintext}")
-        else:
-            print("\nNo match found using the provided hash type and wordlist.")
+        try:
+            decoded_value = decode_base45(hash_value)
+            print("Decoded using Base45:")
+        except:
+            pass
+        
+        try:
+            decoded_value = decode_base58(hash_value)
+            print("Decoded using Base58:")
+        except:
+            pass
+        
+        try:
+            decoded_value = decode_base62(hash_value)
+            print("Decoded using Base62:")
+        except:
+            pass
+        
+        try:
+            decoded_value = decode_base64(hash_value)
+            print("Decoded using Base64:")
+        except:
+            pass
+        
+        
+    if decoded_value is not None:
+        print(decoded_value)
+    else:
+        print("No match or decoding available.")
