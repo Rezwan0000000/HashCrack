27d3afd04e98f4a4f0656d95262bd7852f01d486
Md.Rezwan <87122824+Rezwan0000000@users.noreply.github.com>
2023-09-01 11:04:57 +0600

    Update Hash_crack.py

--- Hash_crack.py
+++ Hash_crack.py
@@ -2,8 +2,6 @@ import hashlib
 import base58
 import base64
 import os
-import html
-import urllib.parse
 def find_hash_match(hash_type, hash_value, wordlist_path):
     with open(wordlist_path, 'r', errors='ignore') as wordlist:
         for line in wordlist:
@@ -43,12 +41,6 @@ def decode_base58(encoded_value):
 
 def decode_base64(encoded_value):
     return base64.b64decode(encoded_value).decode()
-
-def decode_html(encoded_value):
-    return html.unescape(encoded_value)
-
-def decode_url(encoded_value):
-    return urllib.parse.unquote(encoded_value)
 
 def clear_terminal():
     os.system('clear')  # For Linux/macOS
@@ -103,18 +95,6 @@ if __name__ == "__main__":
         try:
             decoded_value = decode_base64(hash_value)
             print("Decoded using Base64:")
-        except:
-            pass
-
-        try:
-            decoded_value = decode_html(hash_value)
-            print("Decoded using HTML:")
-        except:
-            pass
-            
-        try:
-            decoded_value = decode_url(hash_value)
-            print("Decoded using URL:")
         except:
             pass
         
